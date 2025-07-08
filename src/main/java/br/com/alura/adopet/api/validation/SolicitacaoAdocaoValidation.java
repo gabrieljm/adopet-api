256e6fab685837de0947948456ca594647e215a6
Gabriel Matias <gabrieljmatias@gmail.com>
2025-07-08 14:18:40 +0100

    applied strategy and chain of responsabilities to extract validation rules

---
+++ src/main/java/br/com/alura/adopet/api/validation/SolicitacaoAdocaoValidation.java
@@ -0,0 +1,8 @@
+package br.com.alura.adopet.api.validation;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+
+public interface SolicitacaoAdocaoValidation {
+
+    void validar(SolicitacaoAdocaoDto dto);
+}
